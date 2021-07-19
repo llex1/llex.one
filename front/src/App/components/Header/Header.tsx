ac758575db04b7dd66bfef7c8aedc1670fc9deeb
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-07-20 01:44:10 +0300

    header_start

--- front/src/App/components/Header/Header.tsx
+++ front/src/App/components/Header/Header.tsx
@@ -1,15 +1,21 @@
-import Logo from '../Logo'
-
-
-
+import { NavLink } from "react-router-dom";
 
 import styles from './Header.module.scss';
+
+
+
 
 function Header() {
 
   return (
-    <header className='container'>
-      <Logo></Logo>
+    <header className={`container ${styles.header}`}>
+      <NavLink to='/' className={styles.logo}></NavLink>
+      <ul>
+        <li><NavLink to='/'>About</NavLink></li>
+        <li><NavLink to='/'>Services</NavLink></li>
+        <li><NavLink to='/'>Works</NavLink></li>
+        <li><NavLink to='/'>Contacts</NavLink></li>
+      </ul>
       {/* <div className={`container ${styles.header}`}>llex.one</div> */}
     </header>
   )
