a6f04fda6561c04ed3300a770b3dc380ad49ef52
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-07-24 23:44:19 +0300

    icons

--- front/src/App/components/Services/Services.tsx
+++ front/src/App/components/Services/Services.tsx
@@ -1,4 +1,5 @@
 import styles from './Services.module.scss';
+import icons from '../../assets/img/icons.svg';
 
 function Services() {
 
@@ -8,7 +9,19 @@ function Services() {
       <p className={styles.servicesTitle}>Services</p>
       <h2 className={styles.servicesH2}>What  actually I love to do</h2>
       <ul>
-        <li></li>
+        <li>
+          <p><span>
+            <svg className={styles.iconApi} width="62px">
+              <use href={`${icons}#services`}></use>
+            </svg>
+            <svg className={styles.iconApi} width="62px">
+              <use href={`${icons}#api`}></use>
+            </svg>
+            <svg className={styles.iconApi} width="62px">
+              <use href={`${icons}#code`}></use>
+            </svg>
+          </span>API Design</p>
+        </li>
         <li></li>
         <li></li>
       </ul>
