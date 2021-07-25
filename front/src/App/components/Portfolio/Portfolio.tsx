9e155b34a123c2cfe03fde5cfc76b13c860a7279
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-07-25 23:45:37 +0300

    portfolio

--- front/src/App/components/Portfolio/Portfolio.tsx
+++ front/src/App/components/Portfolio/Portfolio.tsx
@@ -1,7 +1,20 @@
+import styles from './Portfolio.module.scss';
+
 function Portfolio() {
 
   return (
-    <p>Portfolio</p>
+    <section className={styles.portfolio}>
+      <div className='container'>
+        <p className={styles.portfolioTitle}>Portfolio</p>
+        <div className={styles.textBlock}>
+          <h2 className={styles.textBlockH2}>All Creative
+            Projects</h2>
+            <p className={styles.textBlockShowMore}>Explore More</p>
+        </div>
+        
+
+      </div>
+    </section>
   )
 }
 
