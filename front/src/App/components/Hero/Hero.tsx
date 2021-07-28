0a9eb1bc0cc97c2b4214ab20da14c6a1748181e1
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-07-28 23:48:30 +0300

    remake_hero

--- front/src/App/components/Hero/Hero.tsx
+++ front/src/App/components/Hero/Hero.tsx
@@ -12,22 +12,32 @@ function Hero(props: any) {
       <p className={styles.heroText}> Full Stack Developer</p>
       <button className={styles.heroButton}>Let’s Talk</button>
 
-      <span className={`${styles.bg} ${styles.bgYellow}`}></span>
+      <svg className={styles.polygonOne}>
+        <use href={`${icons}#polygon1`}></use>
+      </svg>
+      <svg className={styles.polygonTwo}>
+        <use href={`${icons}#polygon2`}></use>
+      </svg>
+      {/* <span className={`${styles.bg} ${styles.bgYellow}`}></span>
       <span className={`${styles.bg} ${styles.bgPink}`}></span>
       <span className={`${styles.bg} ${styles.bgBlue}`}></span>
-      <span className={`${styles.bg} ${styles.bgOrange}`}></span>
+      <span className={`${styles.bg} ${styles.bgOrange}`}></span> */}
       <picture>
         {/* <source srcset="./images/......webp 1x, ./images/.....@2x.webp 2x" media="(min-width: 1440px)" type="image/webp"> */}
         <source srcSet={`${heroImg} 1x, ${heroImg2x} 2x`} media="(min-width: 1440px)" />
         <img className={styles.heroImg} src={heroImg} alt="The boy is at the computer." />
       </picture>
 
-      <svg className={styles.iconWavePink} width="62px">
+      <svg className={`${styles.wave} ${styles.wavePink}`} width="62px">
         <use href={`${icons}#wave`}></use>
       </svg>
-      <svg className={styles.iconWaveGreen} width="48px">
+      <svg className={`${styles.wave} ${styles.waveGreen}`} width="48px">
         <use href={`${icons}#wave`}></use>
       </svg>
+      <svg className={`${styles.wave} ${styles.waveWhite}`} width="48px">
+        <use href={`${icons}#wave`}></use>
+      </svg>
+
 
       <span className={styles.dots}></span>
       <span className={`${styles.circle} ${styles.circleOne}`}></span>
