fbd1076e03f98c3022721d72f5f9025f832545f4
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-08-02 20:45:37 +0300

    footer

--- front/src/App/components/Footer/Footer.tsx
+++ front/src/App/components/Footer/Footer.tsx
@@ -6,9 +6,7 @@ function Footer() {
       <svg className={styles.footerBgWave} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
       <path fill="#E3EFFF" fill-opacity="1" d="M0,320L120,293.3C240,267,480,213,720,186.7C960,160,1200,160,1320,160L1440,160L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
       </svg>
-      <div className="container">
-        footer
-      </div>
+
     </footer>
   )
 }
