9322beb8ba4884cfe6b2e009280b18d2bb198bc7
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-08-04 02:54:57 +0300

    footer

--- front/src/App/components/Footer/Footer.tsx
+++ front/src/App/components/Footer/Footer.tsx
@@ -1,18 +1,29 @@
 
+
 import icons from '../../assets/img/icons.svg';
 import styles from './Footer.module.scss';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path fill="#F6F8FF" fill-opacity="1" d="M0,192L60,202.7C120,213,240,235,360,245.3C480,256,600,256,720,250.7C840,245,960,235,1080,192C1200,149,1320,75,1380,37.3L1440,0L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
-        </path>
-      </svg> */}
-      <div className="container">
+      <svg width="100%" height="320" className={styles.footerBg}>
+        <use href={`${icons}#footer`}></use>
+      </svg>
+      <div className={`container ${styles.footerContainer}`}>
         <p className={styles.footerTitle}>Contact Me</p>
-
-
+        <div className={styles.textBlock}>
+          <h2 className={styles.textBlockH2}>Let me know if you want to talk about a potential collaboration. I'm available for freelance work.</h2>
+          <a href="mailto:alex.tsurka@gmail.com" className={styles.textBlockEmail}>alex.tsurka@gmail.com</a>
+        </div>
+        <form className={styles.footerForm} action="/">
+          <input type="text" placeholder="What’s your name?" />
+          <input type="text" placeholder="Your email" />
+          <input type="text" placeholder="Tell me about your project" />
+          <div className={styles.buttonBlock}>
+            <input className={styles.button} type="submit" />
+            <input type="file" name="" id="" />
+          </div>
+        </form>
       </div>
     </footer>
   )
