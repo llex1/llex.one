8665661dbb002569ef15adb19bac9d1533673243
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-08-17 23:33:15 +0300

    mongo

--- back/index.js
+++ back/index.js
@@ -1,32 +1,34 @@
-const express  = require('express');
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const db = new MongoClient(process.env.DB);
+console.log(MongoClient.toString());
 
 class Server {
-  constructor(){
+  constructor() {
     this.demon = null;
-    this.PORT  = process.env.PORT;
+    this.PORT = process.env.PORT;
   }
-  initDemon(){
-    this.demon = express()
+  initDemon() {
+    this.demon = express();
   }
-  initMiddlewares(){
-    this.demon.use(express.json())
+  initMiddlewares() {
+    this.demon.use(express.json());
   }
-  initRoutes(){
-    this.demon.use('/', express .static('/'))
+  initRoutes() {
+    this.demon.use("/", express.static("/"));
   }
-  runServre(){
-    this.initDemon()
-    this.initMiddlewares()
-    this.initRoutes()
-    this.demon.listen(this.PORT, () => console.log('server is running on port 8080'))
+  runServre() {
+    this.initDemon();
+    this.initMiddlewares();
+    this.initRoutes();
+    this.demon.listen(this.PORT, () =>
+      console.log("server is running on port 8080")
+    );
   }
 }
 
 // new Server().runServre()
-
-
- 
-
 
 // ==================================
