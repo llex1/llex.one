d64c790d8f308c6690b41c55e3e409e4dc5ed13a
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-08-19 20:10:16 +0300

    continuing_make_server

--- back/index.js
+++ back/index.js
@@ -1,27 +1,48 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const {createServer} = require('https');
-const connect = require('connect')
+const { createServer } = require("https");
+const connect = require("connect");
+const cors = require("cors");
+const vhost = require("vhost");
 
-const ssl = require('./assets/ssl')
+const ssl = require("./assets/ssl");
+
+const App_llexOne = require('./llexOne')
+const App_llexOne_API = require('./llexOneAPI')
+
+
 
 class Server {
-  constructor(){
-    this.app = null
-    this.server = null
-    this.socket = null
+  constructor() {
+    this.app = null;
+    this.server = null;
+    this.socket = null;
   }
-  initApp(){
-
+  initApp() {
+    this.app = connect();
   }
-  initServer(){
-    
+  initServer() {
+    this.server = createServer(this.app);
+    this.server.addContext("llex.one", ssl.llexOne);
+    this.server.addContext("api.llex.one", ssl.apiLlexOne);
   }
-
-
-
-
-
-
+  initMiddlewares() {
+    this.app.use(
+      cors({
+        origin: "*",
+      })
+    );
+    this.app.use(vhost('llex.one',App_llexOne));
+  }
+  runServer(){
+    this.initApp()
+    this.initServer()
+    this.initMiddlewares()
+    this.server.listen(process.env.PORT || '8080', ()=>{
+      console.log('server is running');
+    })
+  }
 }
 
+
+new Server().runServer()
---
+++ back/llexOneAPI/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+class App {
+  constructor(){
+    this.app = null
+  }
+  initApp(){
+    this.app = express()
+  }
+  initMiddlewares(){
+  }
+  initRoutes(){
+    // this.app.use(express.static('../'));
+  }
+  run(){
+    this.initApp()
+    this.initMiddlewares()
+    this.initRoutes()
+    return this.app
+  }
+}
+
+module.exports = new App().run()
