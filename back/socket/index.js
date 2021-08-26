7b57b6108829c8a5e3127ac6a5e21971852a26c7
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-08-26 23:58:27 +0300

    socket

--- back/socket/index.js
+++ back/socket/index.js
@@ -1,7 +1,20 @@
-function socketHub(socket) {
-  socket.on("connect", (client) => {
-    console.log(client.id);
-  });
+// function socketHub(socket) {
+//   socket.on("connect", (client) => {
+//     console.log(client.id);
+//   });
+// }
+
+class SocketHub{
+  constructor(){
+    this.socket = null
+  }
+  initSochet(arg){
+    this.socket = arg
+  }
+  runSocket(){
+
+  }
 }
+
 
 module.exports = socketHub;
