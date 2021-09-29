bb012007319c9a52e0dd0507a7e1b79528952731
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-09-29 15:38:27 +0300

    header

--- front/src/App/components/Header/Header.tsx
+++ front/src/App/components/Header/Header.tsx
@@ -10,13 +10,14 @@ function Header() {
     <header className={`container ${styles.header}`}>
       <Link to='/' className={styles.logo}></Link>
       <ul className={styles.navlist}>
-        <li><Link to='/'>About</Link></li>
-        <li><Link to='/'>Works</Link></li>
-        <li><Link to='/'>Contacts</Link></li>
-        {/* <li><Link to='/'>Services</Link></li> */}
+        <li className={styles.navlistItem}><Link to='/'>About</Link></li>
+        <li className={styles.navlistItem}><Link to='/'>Services</Link></li>
+        <li className={styles.navlistItem}><Link to='/'>Works</Link></li>
+        <li className={styles.navlistItem}><Link to='/'>{document.documentElement.clientWidth}</Link></li>
       </ul>
       {/* <button className={styles.buttonMenu} onClick={()=>handleBurgerMenu(!isBurgerMenuOpen)} ><span className={`${styles.burgerIcon} ${isBurgerMenuOpen && styles.close}`}></span></button> */}
       {/* <div className={`container ${styles.header}`}>llex.one</div> */}
+      <button className={styles.navButton}><span className={styles.navButtonIcon}></span></button>
     </header>
   )
 }
