ecb94a3e883e96990db02fd7253b8acb95259601
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-10-05 06:16:17 +0300

    skills

--- front/src/App/components/Skills/Skills.tsx
+++ front/src/App/components/Skills/Skills.tsx
@@ -1,4 +1,5 @@
-import styles from './Skills.module.scss'
+import styles from './Skills.module.scss';
+import icons from '../../assets/img/icons.svg';
 
 
 function Skills() {
@@ -6,6 +7,50 @@ function Skills() {
     <section className={`container ${styles.skills}`}>
       <p className={styles.title}>Skills</p>
       <h2 className={styles.h2}>What  actually I love to do</h2>
+
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <p className={styles.listItemTitle}>
+            <svg className={styles.polygonBack}>
+              <use href={`${icons}#polygonBack`}></use>
+            </svg>
+            <svg className={styles.polygonFront}>
+              <use href={`${icons}#polygonFront`}></use>
+            </svg>
+            UX Research
+          </p>
+          <p className={styles.listItemText}>New demos are continually added and buying a single license for Mak gives you access to all of what's shown below, plus everything that will be added in the future.</p>
+        </li>
+
+
+
+        <li className={styles.listItem}>
+          <p className={styles.listItemTitle}>
+            <svg className={styles.polygonBack}>
+              <use href={`${icons}#polygonBack`}></use>
+            </svg>
+            <svg className={styles.polygonFront}>
+              <use href={`${icons}#polygonFront`}></use>
+            </svg>
+            UX Research
+          </p>
+          <p className={styles.listItemText}>New demos are continually added and buying a single license for Mak gives you access to all of what's shown below, plus everything that will be added in the future.</p>
+        </li>
+        <li className={styles.listItem}>
+          <p className={styles.listItemTitle}>
+            <svg className={styles.polygonBack}>
+              <use href={`${icons}#polygonBack`}></use>
+            </svg>
+            <svg className={styles.polygonFront}>
+              <use href={`${icons}#polygonFront`}></use>
+            </svg>
+            UX Research
+          </p>
+          <p className={styles.listItemText}>New demos are continually added and buying a single license for Mak gives you access to all of what's shown below, plus everything that will be added in the future.</p>
+        </li>
+      </ul>
+
+
     </section>
   )
 }
