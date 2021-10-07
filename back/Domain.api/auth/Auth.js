8febac9b5ac83e7f4cdc03555ed5ee737542d8c0
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-10-07 23:55:44 +0300

    auth_clear

--- back/Domain.api/auth/Auth.js
+++ back/Domain.api/auth/Auth.js
@@ -11,19 +11,6 @@ class Auth {
     const { email, pass } = req.body;
     return db.collection(this.collection).insertOne({ email: email, pass: pass });
   }
-
-
-  async info(req) {
-    // let result;
-    // try {
-    //   result = await req.app.locals.db
-    //     .collection("users")
-    //     .findOne({ '"name"': "Alex" });
-    // } catch (err) {
-    //   console.log("[\x1b[31m ERR \x1b[30m] db connection /Auth.info");
-    // }
-    // console.log(result);
-  }
 }
 module.exports = new Auth();
 
