1ffb017d598b185d654428371e4c2874ed1d659f
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-10-07 18:55:36 +0300

    register

--- back/Domain.api/auth/Auth.js
+++ back/Domain.api/auth/Auth.js
@@ -1,16 +1,18 @@
 class Auth {
-  collection = process.env.DB1Collection1
+  collection = process.env.DB1Collection1;
 
-  register(req){
+  isExistEmail(req) {
     const db = req.app.locals.db;
-    const {email, pass} = req.body
-    return db.collection(this.collection).findOne({email: email})
+    const { email } = req.body;
+    return db.collection(this.collection).findOne({ email: email });
+  }
+  register(req) {
+    const db = req.app.locals.db;
+    const { email, pass } = req.body;
+    return db.collection(this.collection).insertOne({ email: email, pass: pass });
   }
 
-
-
-
-  async info(reqdb) {
+  async info(req) {
     // let result;
     // try {
     //   result = await req.app.locals.db
--- back/Domain.api/auth/auth.controller.js
+++ back/Domain.api/auth/auth.controller.js
@@ -1,23 +1,26 @@
 const AuthModel = require("./Auth");
+const bcrypt = require('bcrypt')
 
 class AuthController {
-
   //register------------------------------------
   async register(req, res, next) {
-    console.log('register Conteroller Start');
+    // if (await AuthModel.isExistEmail(req)) {
+    //   return res
+    //     .status("400")
+    //     .send({ ok: false, message: "This is email not unique." });
+    // }
+    req.body.pass = await bcrypt.hash(req.body.pass, 8)
+    const result =  await AuthModel.register(req)
+    res.status('200').send(result)
 
-    const result = await AuthModel.register(req);
-    console.log(result);
-    
 
 
-    res.status('200').send(result )
+
     next();
   }
+
   //login--------------------------------------
   login(req, res, next) {
-    
-    
     next();
   }
   //logout-------------------------------------
