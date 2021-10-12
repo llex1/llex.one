326b63571d3ff3a9f4393cd340eb38d6ccfad57f
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-10-12 23:26:36 +0300

    footer

--- front/src/App/components/Footer/Footer.tsx
+++ front/src/App/components/Footer/Footer.tsx
@@ -5,14 +5,14 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className='container'>
-        {/* <p className={styles.title}>Contact Me</p>
-        <p className={styles.footerText}>Let me know if you want to talk about a potential collaboration. I'm available for freelance work.</p> */}
-        {/* <form className={styles.footerForm}>
+        <p className={styles.title}>Contact Me</p>
+        <p className={styles.footerText}>Let me know if you want to talk about a potential collaboration. I'm available for freelance work.</p>
+        <form className={styles.footerForm}>
           <input className={styles.formInput} type="text" name="" id="" />
           <input className={styles.formInput} type="text" name="" id="" />
           <input className={styles.formInput} type="text" name="" id="" />
           <button className={styles.footerButton}>Send</button>
-        </form> */}
+        </form>
 
       </div>
     </footer>
