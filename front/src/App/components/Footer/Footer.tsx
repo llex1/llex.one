9cada3b3d2ac9726c1cbfdd067305cf4b1137320
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-10-26 23:17:30 +0300

    footer1

--- front/src/App/components/Footer/Footer.tsx
+++ front/src/App/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 
 function Footer() {
-  
+
   return (
     <footer className={styles.footer}>
       <div className='container'>
@@ -13,6 +13,12 @@ function Footer() {
           <Link to="/portfolio" className={styles.more}>
             <span className={styles.moreArrow}></span>
           </Link>
+        </div>
+        <div className={'styles.formBlock'}>
+          <form className={'styles.form'}>
+            <input className={'styles.formInput'} id="" type="text" />
+
+          </form>
         </div>
       </div>
     </footer>
