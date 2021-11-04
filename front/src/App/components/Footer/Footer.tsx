1ec39dfaca9493246abf4ae558f59a6549fd36f6
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-11-04 18:22:28 +0200

    footer

--- front/src/App/components/Footer/Footer.tsx
+++ front/src/App/components/Footer/Footer.tsx
@@ -12,14 +12,20 @@ function Footer() {
     <footer className={styles.footer}>
       <div className='container'>
         <p className={styles.title}>Contact Me</p>
+        {/* -------------top block---------------- */}
         <div className={styles.topBlock}>
           <p className={styles.footerText}>Let me know if you want to talk about a potential collaboration. I'm available for freelance work.</p>
           <Link to="/portfolio" className={styles.more}>
             <span className={styles.moreArrow}></span>
           </Link>
         </div>
-        <div className={styles.formBlock}>
-        </div>
+        {/* -------------form block---------------- */}
+        <form className={styles.formBlock}>
+          <input type="text" className={styles.inputText} placeholder="What’s your name?" />
+          <input type="text" className={styles.inputText} placeholder="Your email" />
+          <input type="text" className={styles.inputText} placeholder="Tell me about your project" />
+          <input type="submit" className={styles.footerButton} value="Get a Quote" />
+        </form>
       </div>
     </footer>
   )
