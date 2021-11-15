efb4f4f12f507078486ceb7c3bb2fb093957771a
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-11-15 23:19:09 +0200

    ssl

--- back/assets/ssl.js
+++ back/assets/ssl.js
@@ -1,24 +1,5 @@
 const {readFileSync} = require('fs')
 
-<<<<<<< HEAD
-// const llexOne = {
-//   key : readFileSync(`${process.env.LLEXONE_KEYPATH}`),
-//   sert : readFileSync(`${process.env.LLEXONE_SERTPATH}`)
-// }
-// const apiLlexOne = {
-//   key : readFileSync(`${process.env.API_LLEXONE_KEYPATH}`),
-//   sert : readFileSync(`${process.env.API_LLEXONE_SERTPATH}`)
-// }
-const localhost = {
-    key: readFileSync(__dirname+'/../localhost+1-key.pem'),
-    sert: readFileSync(__dirname+'/../localhost+1.pem')
-  }
-
-module.exports = {
-  // llexOne,
-  // apiLlexOne,
-  localhost
-=======
 const sslRootDomain = {
   key : readFileSync(__dirname+`${process.env.sslRootDomain_k}`),
   cert : readFileSync(__dirname+`${process.env.sslRootDomain_c}`)
@@ -31,5 +12,4 @@ const sslSub1Domain = {
 module.exports = {
   sslRootDomain,
   sslSub1Domain
->>>>>>> baf2b96bea1aa4cf3ec83d2dbee096783b5ec339
 }
